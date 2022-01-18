4b29a6748e809aa0900fd43cfe1bf606e5ca5838
rajatkumarcse <96794010+rajatkumarcse@users.noreply.github.com>
2022-01-18 21:33:22 +0530

    Create Assignment-2_Output.py

---
+++ Assignment-2_Output.py
@@ -0,0 +1,78 @@
+# Output (Question ---> 1)
+
+# Part (a)
+The length of this input string is 35
+
+# Part (b)
+egaugnal evitisnes esac a si nohtyP
+
+# Part (c)
+a case sensitive
+
+# Part (d)
+Python is object oriented language
+
+# Part (e)
+10
+
+# Part (f)
+Pythonisacasesensitivelanguage
+
+
+# Output (Question ---> 2)
+
+Enter your name:
+Rajat Kumar
+Enter your SID:
+21103062
+Enter your department:
+Computer Science and Engineering
+Enter your CGPA:
+9.9
+Hey, Rajat Kumar Here!
+My SID is 21103062
+I am from Computer Science and Engineering department and my CGPA is 9.9
+
+
+# Output (Question ---> 3)
+
+# Part (a)
+The value of a & b is 8
+
+# Part (b)
+The value of a | b is 58
+
+# Part (c)
+The value of a ^ b is 50
+
+# Part (d)
+The value of a << 2 is 224
+The value of b << 2 is 40
+
+# Part (e)
+The value of a >> 2 is 14
+The value of b >> 4 is 0
+
+
+# Output (Question ---> 4)
+
+Enter number 1: 30
+Enter number 2: 60
+Enter number 3: 50
+60 is greatest
+
+
+# Output (Question ---> 5)
+
+Enter the text :
+My name is Rajat Kumar
+Yes! name is present
+
+
+# Output (Question ---> 6)
+
+Enter length of first side : 10
+Enter length of second side : 12
+Enter length of third side : 14
+Yes! A triangle can be formed
+
